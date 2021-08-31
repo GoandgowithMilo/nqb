f853fad63144cd8783c14c09af3fd30e0681e726
goandgowithmilo <jamesmilo42@gmail.com>
2021-08-31 22:06:26 +1000

    wip globbing

--- text_processing.c
+++ text_processing.c
@@ -31,7 +31,8 @@ char ** tokenize(char * input)
 
     tokens[counter] = NULL;
 
-    return tokens;
+    return globbed(tokens, counter); //TODO note this is currently passing in the length
+	// including the NULL element, might need to cut this one short
 }
 
 // cleanup function for tokenized strings
@@ -47,5 +48,74 @@ void free_tokens(char ** tokens)
     free(tokens);
 
     return;
+}
+
+// matches all wildcard patterns and returns an array of strings including them
+char ** globbed(char ** tokens, int token_count)
+{
+	// Step 1 - make new array of length = to tokens
+	// Step 2 - iterate through tokens and attempt to glob each element
+	// Step 2a - if the result of gl_pathc is 0, just add tokens element to new array
+	// Step 2b - if gl_pathc > 0, reallocate memory of new array to extra size then add
+	// all elements to new array
+	// Step 3 - Null terminate new array and return it
+
+	// allocates memory for the array of strings
+	char ** globbed_tokens = malloc((sizeof(char *)) * token_count);
+	char * token;
+
+	// counter for globbed_tokens
+	int new_token_counter = 0;
+
+
+	int i = 0;
+	while(tokens[i] != NULL)
+	{
+		glob_t globbuf;
+
+		glob(tokens[i], 0, NULL, &globbuf);
+
+		// case 1 - no matches >>> do not need to increase memory here
+		if(globbuf.gl_pathc == 0)
+		{
+			// allocates memory for the string
+			token = (char *) malloc(sizeof(strlen(tokens[i])));
+			strcpy(token, tokens[i]);
+			globbed_tokens[new_token_counter] = token;
+
+			new_token_counter++;
+		}
+		// case 2 - add all matches to new arrayb >>> increase memory to store new strings
+		else
+		{
+			// increasing size of memory for tokens
+			realloc(globbed_tokens, sizeof(char *) * (token_count + globbuf.gl_pathc)); //TODO this is not going to allocat ethe correct amount. Either need another counter or a better method for doing this
+
+			for(int k = 0; k < globbuf.gl_pathc; k++)
+			{
+				token = (char *) malloc(sizeof(strlen(globbuf.gl_pathv[k])));
+				strcpy(token, globbuf.gl_pathv[k]);
+				globbed_tokens[new_token_counter] = token;
+
+				new_token_counter++;
+			}
+		}
+
+		i++;
+	}
+
+	globbed_tokens[new_token_counter + 1] = NULL;
+
+	int j = 0;
+	while(globbed_tokens[j] != NULL)
+	{
+		printf("%s\n", globbed_tokens[j]);
+		j++;
+	}
+
+	//TODO should free the original tokens at the end of this since we're no
+	// longer using them
+
+	return tokens;
 }
 
--- text_processing.h
+++ text_processing.h
@@ -3,11 +3,14 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <glob.h>
+#include <stdio.h>
 
 // characters to tokenize on
 #define SEPERATORS "<>:' '~$[]|'\n'"   //TODO review whether these are right seperators
 
 char ** tokenize(char * input);
 void free_tokens(char ** tokens);
+char ** globbed(char ** tokens, int token_count);
 
 #endif
